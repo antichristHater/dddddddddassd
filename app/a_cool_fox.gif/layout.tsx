77b345bb5c1d5db20af0a84bfd064c4780cfc85a
Şakir Potçu <obesegoblin@gmail.com>
2024-04-27 04:57:41 +0300

    fox

---
+++ app/a_cool_fox.gif/layout.tsx
@@ -0,0 +1,9 @@
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html><head><meta name="viewport" content="width=device-width, minimum-scale=0.1"/><title>image (640×768)</title></head><body><img src="https://rediropen.netlify.app/_next/image?url=%2Ffox.gif&amp;w=640&amp;q=75" /></body></html>
+  );
+}
